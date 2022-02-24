995dd4c221241227a6980341411a41f7b7e7fe24
Layne Liu <layne.liu@outlook.com>
2022-02-24 14:51:16 +0100

    Make a project structure for dmf

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='dmf',
+    version='0.1',
+    packages=['src'],
+    url='https://github.com/LayneInNL/dmf',
+    license='Apache-2.0 License',
+    author='Layne Liu',
+    author_email='layne.liu@outlook.com',
+    description='An instance of a dynamic monotone framework for type analysis for Python.',
+    entry_points={
+        'console_scripts': [
+            'dmf = src.main:main'
+        ]
+    }
+)
