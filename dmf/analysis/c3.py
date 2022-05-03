e3f6385802258a5142e7f1ffde1d7e836e932ccd
Layne Liu <layne.liu@outlook.com>
2022-05-03 23:24:03 +0200

    Refactor c3 algorithm

--- dmf/analysis/c3.py
+++ dmf/analysis/c3.py
@@ -12,49 +12,24 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-# https://zhuanlan.zhihu.com/p/151856162
 
-
-def c3MRO(cls):
+def c3(cls):
     if cls is object:
-        # 讨论假设顶层基类为object，递归终止
         return [object]
 
-    # 构造C3-MRO算法的总式，递归开始
-    merge_list = [c3MRO(baseCls) for baseCls in cls.__bases__]
-    merge_list.append(list(cls.__bases__))
-    mro = [cls] + merge(merge_list)
-    return mro
+    return [cls] + merge([c3(base) for base in cls.__bases__])
 
 
-def merge(in_lists):
-    if not in_lists:
-        # 若合并的内容为空，返回空list
-        # 配合下文的排除空list操作，递归终止
+def merge(mro_list):
+    if not any(mro_list):
         return []
-
-    # 遍历要合并的mro
-    for mroList in in_lists:
-        # 取head
-        head = mroList[0]
-        # 遍历要合并的mro（与外一层相同），检查尾中是否有head
-        ### 此处也遍历了被取head的mro，严格地来说不符合标准算法实现
-        ### 但按照多继承中地基础规则（一个类只能被继承一次），
-        ### head不可能在自己地尾中，无影响，若标准实现，反而增加开销
-        for cmpList in in_lists[in_lists.index(mroList) + 1 :]:
-            if head in cmpList[1:]:
-                break
-        else:
-            # 筛选出好head
-            next_list = []
-            for mergeItem in in_lists:
-                if head in mergeItem:
-                    mergeItem.remove(head)
-                if mergeItem:
-                    # 排除空list
-                    next_list.append(mergeItem)
-            # 递归开始
-            return [head] + merge(next_list)
+    for candidate, *_ in mro_list:
+        if all(candidate not in tail for _, *tail in mro_list):
+            return [candidate] + merge(
+                [
+                    tail if head is candidate else [head, *tail]
+                    for head, *tail in mro_list
+                ]
+            )
     else:
-        # 无好head，引发类型错误
-        raise TypeError
+        raise TypeError("No legal mro")
