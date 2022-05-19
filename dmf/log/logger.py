68080b1aad8754b8b221c4f8305a66471fd86d23
Layne Liu <layne.liu@outlook.com>
2022-05-19 21:40:08 +0200

    Refactor logger

--- dmf/log/logger.py
+++ dmf/log/logger.py
@@ -11,47 +11,32 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-import datetime
+
 import logging
+import colorlog
 
-
-class CustomFormatter(logging.Formatter):
-    """Logging colored formatter, adapted from https://stackoverflow.com/a/56944256/3638629"""
-
-    grey = "\x1b[38;21m"
-    blue = "\x1b[38;5;39m"
-    yellow = "\x1b[33;20m"
-    red = "\x1b[38;5;196m"
-    bold_red = "\x1b[31;1m"
-    reset = "\x1b[0m"
-
-    def __init__(self, fmt):
-        super().__init__()
-        self.fmt = fmt
-        self.FORMATS = {
-            logging.DEBUG: self.grey + self.fmt + self.reset,
-            logging.INFO: self.blue + self.fmt + self.reset,
-            logging.WARNING: self.yellow + self.fmt + self.reset,
-            logging.ERROR: self.red + self.fmt + self.reset,
-            logging.CRITICAL: self.bold_red + self.fmt + self.reset,
-        }
-
-    def format(self, record):
-        log_fmt = self.FORMATS.get(record.levelno)
-        formatter = logging.Formatter(log_fmt)
-        return formatter.format(record)
-
-
-# Create custom logger logging all five levels
-logger = logging.getLogger(__name__)
+handler = colorlog.StreamHandler()
+formatter = colorlog.ColoredFormatter(
+    "%(log_color)s%(levelname)-8s%(reset)s %(blue)s%(message)s",
+    datefmt=None,
+    reset=True,
+    log_colors={
+        "DEBUG": "cyan",
+        "INFO": "green",
+        "WARNING": "yellow",
+        "ERROR": "red",
+        "CRITICAL": "purple,bg_blue",
+    },
+    secondary_log_colors={},
+    style="%",
+)
+handler.setFormatter(formatter)
+logger = colorlog.getLogger("dmf")
 logger.setLevel(logging.DEBUG)
-
-# Define format for logs
-fmt = "%(asctime)s | %(levelname)8s | %(message)s"
-
-# Create stdout handler for logging to the console (logs all five levels)
-stdout_handler = logging.StreamHandler()
-stdout_handler.setLevel(logging.DEBUG)
-stdout_handler.setFormatter(CustomFormatter(fmt))
-# Add both handlers to the logger
-logger.addHandler(stdout_handler)
+logger.propagate = False
+logger.addHandler(handler)
+logger.debug("hello")
+logger.warning("warning")
+logger.info("hello")
+logger.error("hello")
+logger.critical("hello")
